60b0666d43739cba885841f833aa4ce5f4520930
unknown <idahosajoshua61@gmail.com>
2022-03-19 03:04:52 +0100

    initials

---
+++ app.js
@@ -0,0 +1,75 @@
+let baseurl = 'https://type.fit/api/quotes'
+let newQuote = document.getElementById('getQuote');
+let tweet = document.getElementById('tweet');
+let authur = document.getElementById('authur'); 
+let quoteText = document.getElementById('text');
+let result = []
+let container = document.getElementById('quote')
+let loader = document.getElementById("load")
+
+//remove loader on load of window
+ window.addEventListener("load", () =>{
+    loader.classList.add('vanish')
+} )
+
+
+
+//generate new quote and render it to the DOM
+function generateQuote(data){
+      newQuote.style.transform = 'rotate(-360deg)'
+      result = data[Math.floor(Math.random()*data.length)];
+    setTimeout(()=>{
+      if (result.text.length > 130) {
+        quoteText.textContent = result.text
+        quoteText.style.fontSize = "18px"
+        container.style.height = "65vh"
+       }else if(result.text.length <50){
+        quoteText.textContent = result.text
+        quoteText.style.fontSize = "20px"
+        container.style.height = "30vh"
+       }
+       else {
+        quoteText.textContent = result.text
+        quoteText.style.fontSize = "20px"
+       container.style.height = "45vh"
+  
+       }
+       if (!result.author) {
+        authur.textContent = "-Rene ✨" 
+       } else {
+        authur.textContent = "-"+result.author  
+       }  
+    },2000)    
+}
+
+//tweet button sends quote to twitter
+function retweet(){
+  let query = `https://twitter.com/intent/tweet/?text=${quoteText.textContent} -${authur.textContent}`
+  window.open( query, '_blank')
+}
+tweet.addEventListener('click', retweet)
+
+//rotates the spinner on click of the button
+ function rotate(){
+   newQuote.style.transform = "rotate(-360deg)" 
+ }
+newQuote.addEventListener('click',getQuote)
+
+//Fetch API
+ async function getQuote(){  
+     try {
+        
+         let response = await fetch(baseurl);
+         let data = await response.json();
+         generateQuote(data);
+     } catch(error) {
+       console.log(error)
+     }
+ }
+
+getQuote()
+
+
+
+
+ 
